c3a67d7331a9738f7711db843465511d1161ea21
Mateus <mateus.fmaciel@uni9.edu.br>
2022-01-01 19:24:44 -0300

    first commit

---
+++ src/main/java/com/fin/finback/FinBackApplication.java
@@ -0,0 +1,13 @@
+package com.fin.finback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fin/finback/FinBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fin.finback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
